872ab76f056982b6c8b36185929eab18a2d3eadc
joseandre <contretaa@gmail.com>
2021-12-22 11:46:07 -0400

    change: header-subject > buttonIcon replace by icon

--- src/components/molecules/HeaderSubjects/HeaderSubjects.js
+++ src/components/molecules/HeaderSubjects/HeaderSubjects.js
@@ -1,22 +1,28 @@
 import Heading from '../../atoms/Heading';
-import ButtonIcon from '../../molecules/ButtonIcon';
+import Icon from '../../atoms/Icon';
 
 import PropTypes from 'prop-types';
 
 import './HeaderSubjects.css';
+import classNames from 'classnames';
 
 const HeaderSubjects = ({ isShowing, setShow }) => {
   //TODO refactor classnames
+  //TODO rename name carreer by career
 
   return (
-    <div className="l-flex-space-between subjects">
+    <div
+      className="l-flex-space-between header-subjects"
+      onClick={() => setShow(!isShowing)}>
       <Heading type="h2" textTransform="uppercase" size="md">
         Subjects of Carreer
       </Heading>
-      <ButtonIcon
-        onClick={() => setShow(!isShowing)}
-        type="tertiary"
-        icon="icon-arrow-up"></ButtonIcon>
+      <span
+        className={classNames('header-subjects__icon ', {
+          isActive: isShowing,
+        })}>
+        <Icon type="icon-arrow-up" size="sm" />
+      </span>
     </div>
   );
 };
