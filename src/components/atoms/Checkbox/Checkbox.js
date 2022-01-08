5d8ab720a562466a7d7b371672de3cd034c0ae10
joseandre <contretaa@gmail.com>
2022-01-07 22:31:05 -0400

    style: replace custom checkbox styles to native property of css for change color box

--- src/components/atoms/Checkbox/Checkbox.js
+++ src/components/atoms/Checkbox/Checkbox.js
@@ -4,10 +4,10 @@ export default function Checkbox({ text, id }) {
   //TODO handler many checkbox
   return (
     <div className="checkbox">
-      <input id={id} type="checkbox" className="checkbox__input" />
       <label className="checkbox__label" htmlFor={id}>
         {text}
       </label>
+      <input id={id} type="checkbox" className="checkbox__input" />
     </div>
   );
 }
